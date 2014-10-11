054cd2fc5672989af8f6e8ae4ebe425847a5b46d
user <user@localhost.localdomain>
2014-10-11 12:14:58 +0900

    シンタックスの警告などを一部除去

--- app/scripts/mock.js
+++ app/scripts/mock.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular
 	.module('ryosanApp')
 		.run(function($httpBackend) {
@@ -9,11 +11,11 @@ angular
 		 */
 		.run(function($httpBackend) {
 			var tenpoItems = [
-				                 { storeCd : 100, storeName : '静内' }
-				                 , { storeCd : 200, storeName : '北見' }
-				                 , { storeCd : 300, storeName : '帯広' }
-				                 , { storeCd : 400, storeName : '旭川' }
-				                 ]
+				                 { storeCd : 100, storeName : '静内' },
+				                 { storeCd : 200, storeName : '北見' },
+				                 { storeCd : 300, storeName : '帯広' },
+				                 { storeCd : 400, storeName : '旭川' }
+							 ];
 
 			$httpBackend.whenGET('store/list').respond(200, tenpoItems);
 		})
@@ -21,11 +23,12 @@ angular
 		 * 役割一覧取得用APIスタブ
 		 */
 		.run(function($httpBackend) {
-			var result = [
-			              {'yakuwariCd' : '1000', 'yakuwariName': '管理者'},
-			              {'yakuwariCd' : '2000', 'yakuwariName': 'オーナー'},
-			              {'yakuwariCd' : '3000', 'yakuwariName': '店長'},
-			              {'yakuwariCd' : '3000', 'yakuwariName': '従業員'}
-			              ];
+			var roleItems = [
+								{ yakuwariCd : '1000', yakuwariName: '管理者' },
+								{ yakuwariCd : '2000', yakuwariName: 'オーナー' },
+								{ yakuwariCd : '3000', yakuwariName: '店長' },
+								{ yakuwariCd : '3000', yakuwariName: '従業員' }
+							];
+			$httpBackend.whenGET('role/list').respond(200, roleItems);
 		})
 		;
